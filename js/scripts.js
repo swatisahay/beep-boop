b5a91044f4eeb84c504be3ca90aba11c33588cd5
Swati Sahay <swatiranjan0111@gmail.com>
2018-06-08 11:56:16 -0700

    add user interface and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,65 +2,34 @@
 $(function() {
 
   $("#submit").click(function(){
-    // debugger
-    var message= $("input#coded").val();
-    var cryptic= cryptosquare(message);
-    $("#output").text(cryptic);
+    var name= $("input#name").val();
+    var number= $("input#num").val();
+    var result= beepBoop(number);
+    $("#output").text(result);
+    $(".names").text(name)
   });
 });
-// business logic
-var cryptosquare=function(message){
-  // removing punchuatin ,special characteristics and lowercasing it
-  message = message.replace(/[.,'?\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-  message = message.replace(/[" "]/g,"");
-  message = message.toLowerCase();
-  // string length
-  var size = message.length;
-  // no.of columns, rows=column+1
-  var len =parseInt(Math.sqrt(size));
-  if(size<=2) {
-    return message;
-  }
-  // debugger
-  if(size<=9){
-    len=3;
-  }
-  // splitting strings into letter and storing it in array
-  var myArray=message.split("");
-  // creating rectangular or square array
-  var array=new Array(len+1);
-  var emptyArray=[];
-  var k=0;
-  for(var i=0 ; i<len+1; i++){
-    array[i]=new Array(len);
-    for(var j=0; j<len; j++){
-      if(k<size) {
-        array[i][j] = myArray[k];
-        k+=1;
-      }
-    }
 
-  }
-  // code for reading elements through columns
-  var k=0;
-  var str="";
-  var coded=[];
-  for(var i=0 ; i<len; i++){
-    for(var j=0; j<len+1; j++){
-      if(array[j][i]!=undefined){
-        str+=array[j][i];
-        k+=1;
-        if(k===5){
-          coded.push(str);
-          k=0;
-          str="";
-        }
+// business Logic
+var beepBoop = function(number){
+  numberString= number.toString();
+  // to print range of numbers
+  var range=[];
+  for (var i=0; i<=number;i+=1){
+   range.push(i);
 
-      }
-    }
-
+// to check if no. is divisible by 3
+  if((number%3===0)&&(number>0)){
+    return("I'm sorry, Dave. I'm afraid I can't do that.");
   }
-  coded.push(str);
-  var encrypted = coded.join(" ");
-  return encrypted;
+  // to check if number includes 1
+  else if((numberString.indexOf('1'))>(-1)){
+    return("boop");
+  }
+  // to check if number includes 0
+  else if((numberString.indexOf('0'))>(-1)) {
+    return("beep")
+  }
+}
+return range;
 }
