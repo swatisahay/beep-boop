e81a79818b128b262ccec760465dca8cd6175f16
Swati Sahay <swatiranjan0111@gmail.com>
2018-06-08 17:23:26 -0700

    css styling

--- js/scripts.js
+++ js/scripts.js
@@ -14,27 +14,27 @@ $(function() {
 var beepBoop = function(number){
   // to print range of numbers
   debugger
-if(number==="") return "Empty Box";
+  if(number==="") return "Empty Box";
   var range=[];
   for (var i=0; i<=number;i+=1){
 
-// to check if no. is divisible by 3
- if((i%3===0)&&(i>0)){
-    range.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    // to check if no. is divisible by 3
+    if((i%3===0)&&(i>0)){
+      range.push("I'm sorry, Dave. I'm afraid I can't do that.");
 
+    }
+    // to check if number includes 1
+    else if(i.toString().indexOf('1')>(-1)){
+      range.push("Boop!");
+    }
+    // to check if number includes 0
+    else if(i.toString().indexOf('0')>(-1)) {
+      range.push("Beep!");
+    }
+    else{
+      range.push(i);
+    }
   }
-  // to check if number includes 1
-  else if(i.toString().indexOf('1')>(-1)){
-    range.push("boop");
-  }
-  // to check if number includes 0
-  else if(i.toString().indexOf('0')>(-1)) {
-    range.push("beep");
-  }
-  else{
-    range.push(i);
-  }
-}
-range = range .join(", ");
-return range;
+  range = range .join(", ");
+  return range;
 }
